28bf9593a5dbce0f5ca97957c465152c475561e4
dhanashripatil205 <107808336+dhanashripatil205@users.noreply.github.com>
2023-03-16 17:26:05 +0530

    UC3-Authorisation-FundooNotes-CRUD!!!

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -6,7 +6,7 @@ import { userAuth } from '../middlewares/auth.middleware';
 const router = express.Router();
 
 // //route to create a new user
-router.post('', newUserValidator, userController.userRegistration);
+router.post('', newUserValidator, userAuth, userController.userRegistration);
 
 //route to get a single user by their user id
 router.get('/login', userAuth, userController.userLogin);
