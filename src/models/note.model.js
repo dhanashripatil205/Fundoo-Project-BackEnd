cf6b4bd04d1a8dc1e92c5c0a56ea475131d5938b
dhanashripatil205 <107808336+dhanashripatil205@users.noreply.github.com>
2023-03-22 17:15:29 +0530

    UC5-Forgot-And-Reset-Password!!!

---
+++ src/models/note.model.js
@@ -0,0 +1,44 @@
+import {Schema, model } from 'mongoose';
+
+const noteSchema = new Schema(
+  {
+    title: 
+    {
+      type: String
+    },
+
+    description: 
+    {
+      type: String
+    },
+
+    archieve: 
+    {
+      type: Boolean,
+      default:false
+    },
+
+    trash: 
+    {
+      type: Boolean,
+      default:false
+    },
+
+    userId: 
+    {
+      type: String
+    },
+
+    color: 
+    {
+      type: String
+    },
+    
+    
+  },
+  {
+    timestamps: true
+  }
+);
+
+export default model('Note', noteSchema);
